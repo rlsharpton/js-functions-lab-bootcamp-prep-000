40272baffcf297f968e63ac926e8955f53a01a43
Robin Sharpton <robin@robinls.net>
2018-06-20 12:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return happyHolidays
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
